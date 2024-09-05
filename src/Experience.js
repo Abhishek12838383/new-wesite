e32b68beb05ef31648099952509e24f99dc72ae0
Abhishek12838383 <105066093+Abhishek12838383@users.noreply.github.com>
2024-09-05 14:46:26 +0530

    Add files via upload

---
+++ src/Experience.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div>
+      
+      {/* para */}
+<div className=' text-[#969696] w-[30vw] ml-3 mt-3 text-sm max-xl:text-xs max-lg:text-[10px]'>
+Hello! I’m Dave, your sales rep here from Salesforce. I’ve been working at this awesome company for 3 years now.
+</div>
+
+<div className='  text-[#969696] w-[31vw] ml-3 text-sm mt-5 max-xl:text-xs max-lg:text-[11px]'>
+I was born and raised in Albany, NY& have been living in Santa Carla for the past 10 years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a...
+</div>
+    </div>
+  )
+}
+
+export default Experience
